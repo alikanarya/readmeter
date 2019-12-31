8cb2b51cd384eb28e35c26a30fbb0b0947b2ae08
alikanarya <akanarya@yahoo.com>
2019-12-31 18:16:05 +0300

    1.0 Hello world

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    printf("Hello World");
+    cout << endl;
 
     return a.exec();
 }
--- readmeter.pro
+++ readmeter.pro
@@ -4,6 +4,11 @@ QT -= gui
 CONFIG += c++11
 
 TARGET = readmeter
+target.files = readmeter
+target.path=/root/app
+
+INSTALLS += target
+
 CONFIG += console
 CONFIG -= app_bundle
 
